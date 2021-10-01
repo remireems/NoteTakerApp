ad99726f35c362b847d32d20a1a4167ad10b697d
Remi <75871744+remireems@users.noreply.github.com>
2021-09-30 20:32:07 -0700

    added code

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { join } = require('path')
+
+const app = express()
+
+app.use(express.static(join.(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(require('./routes'))
+
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, './public/index.html'))
+})
+
+app.listen(process.env.PORT || 3000)
