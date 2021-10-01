fb65ff56a118d1bff657ac9cf379c34fed87f6f4
Remi <75871744+remireems@users.noreply.github.com>
2021-09-30 21:28:40 -0700

    fixed code

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { join } = require('path')
 
 const app = express()
 
-app.use(express.static(join.(__dirname, 'public')))
+app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
